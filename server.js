ff13cbcf5890fce79773139991234b3a40706eaf
jennnguyen <jennnguyen@csumb.edu>
2018-09-27 21:26:19 -0700

    updates to heroku deploy

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -6,11 +9,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/your_app_name'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
